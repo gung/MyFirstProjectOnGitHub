cb46cb34cb15fdbfa63f3dd124b11f62fdf01e8d
gung <gung@inbox.ru>
2012-04-09 17:36:04 +0300

    иду домой

--- JavaScriptCode/javascript.js
+++ JavaScriptCode/javascript.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-        $("#output").append("Jquery init complete");
+        $("#output").append("Jquery init complete<br>");
+        for(var v in this){
+            $("#output").append(v+":"+this[v]+"<br>");
+        }
     });
